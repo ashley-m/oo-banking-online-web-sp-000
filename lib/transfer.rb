aa930698b42a8000c7231b4365994bfe3fa40fb6
Ashley Miller <asm2fe@virginia.edu>
2019-04-14 19:21:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,12 +15,13 @@ class Transfer
   end
 
   def execute_transaction
-    binding.pry
+    #binding.pry
     if @status == "pending" && valid?
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
     else
+      @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
   end
