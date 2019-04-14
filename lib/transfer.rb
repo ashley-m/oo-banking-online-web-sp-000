4bb2102fc9c19fca518657e7a6e0ec1070c4f671
Ashley Miller <asm2fe@virginia.edu>
2019-04-14 19:13:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,10 @@
 class Transfer
 
-  attr_accessor :sender, :receiver, :amount
+  attr_accessor :sender, :receiver, :amount, :status
   def initialize(acct1, acct2, amount)
     @sender = acct1
     @receiver = acct2
     @amount = amount
+    @status = "pending"
   end
 end
