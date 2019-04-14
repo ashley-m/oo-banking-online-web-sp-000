2a811fba2a034fb7cca34a43d81da45a08e0e3c4
Ashley Miller <asm2fe@virginia.edu>
2019-04-14 19:21:32 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
 
   def execute_transaction
-    #binding.pry
+    binding.pry
     if @status == "pending" && valid?
       @sender.balance -= @amount
       @receiver.balance += @amount
