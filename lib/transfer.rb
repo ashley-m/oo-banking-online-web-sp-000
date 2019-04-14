7f0f8c4fc42bf69d8f5ad6a018cd99954902750b
Ashley Miller <asm2fe@virginia.edu>
2019-04-14 19:17:32 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,6 +17,7 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    end
+    else
+      return "Transaction rejected. Please check your account balance."
   end
 end
