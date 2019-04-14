2a5d9f7821a37902c724999559acc7d0203215fe
Ashley Miller <asm2fe@virginia.edu>
2019-04-14 19:18:25 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Transfer
 
   attr_accessor :sender, :receiver, :amount, :status
@@ -13,6 +15,7 @@ class Transfer
   end
 
   def execute_transaction
+    binding.pry
     if @status == "pending" && valid?
       @sender.balance -= @amount
       @receiver.balance += @amount
